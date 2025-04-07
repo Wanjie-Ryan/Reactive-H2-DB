1bbcd63bb47ba3e47f29ab06292915d896fa9204
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2025-04-07 14:41:38 +0300

    added h2-console

---
+++ src/main/java/com/blog/reactive/controller/userRest.java
@@ -0,0 +1,53 @@
+package com.blog.reactive.controller;
+
+import java.util.UUID;
+
+public class userRest {
+
+    private UUID id;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public userRest() {
+    }
+
+    public userRest(UUID id, String firstname, String lastname, String email) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
